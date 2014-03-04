99854eb8e865b0358f7d215bf66b6f1f8b6f27b0
Antti J Karikoski <anttkari@cs@da1-cs-bk107-25.helsinki.fi>
2014-03-04 13:53:37 +0200

    rails app initialized, first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FifaChampions::Application.config.secret_key_base = '683a5f8066ef8853c54d822f1a014a0024ee6773e33ac19e512e757d692f0380b6a10f29e19159451c7807f2a5677ba7c1fc1afdaf1784f71815f131910c3364'
