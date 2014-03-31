1b9933c0bda87098bd0df9b43c72649299d6f1f9
Karikoski Antti J <anttkari@da3-cs-dk110-02.helsinki.fi>
2014-03-31 14:18:07 +0300

    dependent destroy for tournament stats when tournament is destroyed

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -1,5 +1,5 @@
 class Tournament < ActiveRecord::Base
-	has_many :tournament_stats
+	has_many :tournament_stats, dependent: :destroy
 	has_many :users, through: :tournament_stats
 
 	validates :name, uniqueness: true, length: {minimum: 3, maximum: 15}
